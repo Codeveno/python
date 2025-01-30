5d2c98593ace3533925f7e6d3309c662ea2e373a
codeveno <samsonochieng476@gmail.com>
2025-01-30 19:45:21 +0300

    typecasting

---
+++ typecasting.py
@@ -0,0 +1,17 @@
+print(len("samson"))
+
+length=len("samson")
+print("your name has", str(length)+ "characters")
+
+
+print(type(length))
+
+new_lenth=str(length)
+print(type(new_lenth))
+
+#other functions are
+int()
+print("10"+int("10"))#converting string to int
+float()
+str()
+
