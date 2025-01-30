418ea4314f095358a0208c8ce152ef9843a1a485
codeveno <samsonochieng476@gmail.com>
2025-01-30 18:40:25 +0300

    //

--- Pdatatypes.py
+++ Pdatatypes.py
@@ -8,3 +8,15 @@ print(0x300)# hexadecimal
 
 
 
+#strings is just character seqquences
+var1=43465237598490314416504273# ANY LONG NUMBER
+var3=0O23
+print(var1)
+var1_1=123
+var2=10.6#float
+print(var2+var1_1)
+print(type(var1_1))
+print(var3)
+#assuming special meanings
+print( 5*"sammy\'s laptops\'\n")
+#we use backslash to evade special meaning of a char or symbol in python
