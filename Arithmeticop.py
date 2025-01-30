d75548ed88d5857434dd7f7e51d361cef8515f7b
codeveno <samsonochieng476@gmail.com>
2025-01-30 20:46:29 +0300

    Arithmeticop

---
+++ Arithmeticop.py
@@ -0,0 +1,13 @@
+#+,-,/,%,*
+print(2/3)
+print(7%2)
+print(2**7)#2 power7
+print(3%9, 2*3,9/3)
+
+
+# calculating BMI
+# BMI=weight/(height*height)
+weight=input("please enter your weight:")
+Height=input("please enter your Height")
+BMI =int(weight)/(int(Height)*int(Height))
+print(BMI)
