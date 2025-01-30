2ae908e9e83057cff38eb634b36b72786a0b5f1e
codeveno <samsonochieng476@gmail.com>
2025-01-30 17:41:11 +0300

    hello and swap numbers

---
+++ Helloworld.py
@@ -0,0 +1,4 @@
+print("hello world")
+
+name1= input(" enter your name")
+print("Nice to meet you" + name1)
---
+++ swapnumbers.py
@@ -0,0 +1,8 @@
+a=input("enter the value of a=")
+b=input("Enter the value of b=")
+
+temp = a
+a=b
+b=10
+print("a=", a)
+print("b=", b)
