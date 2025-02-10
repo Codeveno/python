47e9f0eff39636ed879f78ffeada1876c28742f5
codeveno <samsonochieng476@gmail.com>
2025-02-10 15:17:03 +0300

    pass

---
+++ pass.py
@@ -0,0 +1,8 @@
+#pass is used when a statement is required syntactically but you do not want any command or code to execute.
+
+for letter in 'Python':
+    if letter == 'h':
+        pass
+        print ('This is pass block')
+    print ('Current Letter :', letter)
+    
