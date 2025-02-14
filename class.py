5f22fdbb258565fdbf04c537e32b3a11859864fc
codeveno <samsonochieng476@gmail.com>
2025-02-14 20:29:28 +0300

    class.py

---
+++ class.py
@@ -0,0 +1,20 @@
+#class is used to define a class in python. It is a keyword that marks the start of the class header.
+#example
+class MyClass:
+  x = 5
+#
+# Compare this snippet from pass.py:
+# #pass is a null statement in python. Nothing happens when it is executed. It is used as a placeholder.
+# #example
+ a = 33
+ b = 200
+if b > a:
+      pass
+#
+# Compare this snippet from import.py:
+# #import is used to import a module in python. It is used to include a library module in the current file.
+# #example
+import math
+print(math.pi)
+#
+
