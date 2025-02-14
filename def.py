4dee83764e950acb7e7ae8406f1dab2d12efd9cb
codeveno <samsonochieng476@gmail.com>
2025-02-14 20:21:53 +0300

    def

---
+++ def.py
@@ -0,0 +1,5 @@
+#def word in python is used to define a function in python. It is a keyword that marks the start of the function header.
+#example
+def my_function():
+    print("Hello from a function")
+    
