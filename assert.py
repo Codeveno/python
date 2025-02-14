284691c388fcd55c25cc675d50004ee0defdc57c
codeveno <samsonochieng476@gmail.com>
2025-02-14 20:19:53 +0300

    assert

---
+++ assert.py
@@ -0,0 +1,3 @@
+#assert is used to check the condition. If the condition is true, the program will continue execution. If the condition is false, the program will raise an AssertionError.
+#example
+assert 5 < 3, "5 is not less than 3"
