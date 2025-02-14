3ab3f597d8811ba4aab977dc3528e3cd7cfebdb3
codeveno <samsonochieng476@gmail.com>
2025-02-14 20:26:09 +0300

    lamda

---
+++ lambda.py
@@ -0,0 +1,7 @@
+#lamda is used to create an anonymous function in python. It is a small and restricted function having no more than one line. 
+# It can have any number of arguments but only one expression.
+#example
+x = lambda a : a + 10
+print(x(5))
+
+
