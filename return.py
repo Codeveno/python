8ddb2ab1863521b25da1e1a6e9d46880f9c25169
codeveno <samsonochieng476@gmail.com>
2025-02-14 20:23:51 +0300

    return

---
+++ return.py
@@ -0,0 +1,5 @@
+#return is used to return a value from a function. It is used to exit the function and return a value to the caller function.
+#example
+def my_function():
+    return 5
+print(my_function())
